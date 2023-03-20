89a8ecf6db8293d118c79fced8b76a760c50a8d6
Qaisar Abbas <122449496+qaisar786abbas@users.noreply.github.com>
2023-03-20 18:38:22 +0500

    added files

---
+++ Js-26-30/app.js
@@ -0,0 +1,123 @@
+
+//>>>>>>>>>>>>CHAPTER NO 26 TO 30<<<<<<<<<<<<<<<<
+
+
+//Question no 1
+
+// var number = 4.3546;
+// document.write("Number is:" + number + "</br>");
+// var round = Math.round(number);
+// document.write("Round number:" + round + "</br>");
+// var ceil = Math.ceil(number);
+// document.write("Ceil number:" + ceil + "</br>");
+// var floor = Math.floor(number);
+// document.write("floor number:" + floor);
+
+
+//Question no 02
+
+// var number = prompt("Enter negative floating number.");
+// var round = Math.round(number);
+// document.write("Round number:" + round + "</br>");
+// var ceil = Math.ceil(number);
+// document.write("Ceil number:" + ceil + "</br>");
+// var floor = Math.floor(number);
+// document.write("floor number:" + floor);
+
+
+//Question no 03
+
+// var number = prompt("Enter any number.");
+// var absolute = Math.abs(number);
+// document.write("Absolute number:" + " " + absolute);
+
+
+//Question no 04
+
+
+// var diceValue = Math.floor(Math.random()*6);
+// document.write("Random diceValue is:" + " " + diceValue);
+
+
+//Question no 05
+
+// var toss = Math.floor(Math.random()*2);
+// if(toss == 0){
+//     document.write("Random value of coin is: Tail");
+// }
+// if(toss == 1){
+//         document.write("Random value of coin is: Head");
+// }
+
+//Question no 06
+
+// var toss = Math.floor(Math.random()*100)+1;
+// document.write("Random number between 1 to 100 is :" + " " + toss);
+
+//Question no 07
+//(a)
+// var weight = prompt("Enter your weight.");
+// var kg = parseInt(weight);
+// document.write("Your weightis : " + kg);
+
+//(b)
+// var weight = prompt("Enter your weight.");
+// var kg = parseInt(weight);
+// document.write("Your weight is : " + kg + "kgs");
+
+//(c)
+// var weight = prompt("Enter your weight.");
+// var kg = parseFloat(weight);
+// document.write("Your weight is : " + kg + "kgs");
+
+//(d)
+// var weight = prompt("Enter your weight.");
+// var kg = parseFloat(weight);
+// document.write("Your weight is : " + kg + "kilograms");
+
+
+//Question no 08
+
+// var guessNumber = prompt("Enter any number from 1 to 10");
+
+// var secretNumber = 6;
+
+// if(guessNumber == secretNumber) {
+//     document.write("Congratulations you win.");
+// }
+// else{ 
+//     document.write("Sorry,Try again");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
